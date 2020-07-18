78b65506bb6e174c5c90cf49f207573925305e45
Alan Morales <alan_jesus99@hotmail.com>
2020-07-18 15:12:51 -0500

    added link in logo, corrected link to repository

--- components/GithubBtn.js
+++ components/GithubBtn.js
@@ -6,7 +6,7 @@ import {faGithub} from '@fortawesome/free-brands-svg-icons';
 class GithubBtn extends React.Component {
     render() {
         return(
-            <a href="https://github.com/alanmoraales" target="blank" className={styles.botton}>
+            <a href="https://github.com/alanmoraales/moviefinder-nextjs-omdb" target="blank" className={styles.botton}>
                 <div className={styles.grid}>
                     <div className={styles.githublogo}><FontAwesomeIcon icon={faGithub}/></div>
                     <div>see project on Github</div>
--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,7 @@
 import styles from './Header.module.css';
 
+import Link from 'next/link';
+
 import Logo from '../res/svg/Logo.svg';
 import SearchBar from './SearchBar';
 
@@ -8,7 +10,9 @@ class Header extends React.Component {
     render() {
         return(
             <div className={styles.container}>
-                <div className={styles.logo}><Logo id={styles.logo}/></div>
+                <div className={styles.logo}>
+                    <Link href="/"><Logo id={styles.logo}/></Link>
+                </div>
                 <div className={styles.searchbar}><SearchBar/></div>
             </div>
         )
